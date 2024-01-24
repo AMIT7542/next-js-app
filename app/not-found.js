b018db37b7227a69477f715e69bce642efdd1b68
Amit Kanse <amitkanse1997@gmail.com>
2024-01-24 15:06:53 +0530

    Handled Not Found route

---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <main className='not-found'>
+            <h1>Not Found</h1>
+            <p>Unfortuntly we could not load requested page</p>
+        </main>
+    )
+}
+
+export default NotFound
