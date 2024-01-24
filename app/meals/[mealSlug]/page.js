df507bb7ae21b2f3b3825098db22dc413eee0765
Amit Kanse <amitkanse1997@gmail.com>
2024-01-24 16:31:46 +0530

    ADDED mealdetailPage

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,33 @@
-import React from 'react'
 
+import React from 'react'
+import classes from './page.module.css'
+import Image from 'next/image'
+import { getMeal } from '@/lib/meals'
 const MealDetailPage = ({ params }) => {
-    console.log(params)
+    const data = getMeal(params.mealSlug)
+    console.log(data)
+    const { image, id, slug, title, summary, instructions, creator, creator_email } = data
     return (
-        <div>MealDetailPage {params.mealSlug}</div>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={image} alt={title} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{title}</h1>
+                    <p className={classes.creator}>
+                        By {creator}
+                    </p>
+                    <p className={classes.summary}>
+                        {summary}
+                    </p>
+                </div>
+
+            </header>
+            <main>
+                <p className={classes.instructions}>{instructions}</p>
+            </main>
+        </>
     )
 }
 export default MealDetailPage
